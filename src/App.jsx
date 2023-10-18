cebdd9b51125813ac8f053db5f3a3bc08bf1175a
Shiva Sunchu <shiva@guvi.in>
2023-10-18 16:24:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const arr = [{}, {}];
+  function add() {}
+
+  const helloMessage = "Hello Ankit";
+
+  return (
+    <div className="App">
+      <h1>Hello, {helloMessage}</h1>
+    </div>
+  );
+}
+
+export default App;
